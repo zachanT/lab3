9f1f4a60bc45519bc124b438dc7d1a5930162574
zachanT <zacharyTchan@gmail.com>
2020-01-23 10:42:49 -0800

    updated files

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -3,8 +3,13 @@
  * GET home page.
  */
 
-exports.view = function(req, res){
+exports.view = function (req, res) {
+   var nameToShow = req.params.userName;
+   if (nameToShow === undefined) {
+      nameToShow = 'World';
+   }
+   console.log("Name is " + nameToShow);
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+	   $('.jumbotron h1').text("Javascript is connected");
+	   $("#testjs").text("Changed");
+	   $(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+   // example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+   // prevent the page from reloading
+   e.preventDefault();
+   // In an event handler, $(this) refers to
+   // the object that triggered the event
+   var containingProject = $(this).closest(".project");
+   var description = $(containingProject).find(".project-description");
+   if (description.length == 0) {
+      $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+   } else {
+      $(containingProject).fadeToggle();
+   }
 }
